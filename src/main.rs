1b4dc09e56e3ed4208b17fdc90315f913dfb8586
_Bare <bare@logixsr.com>
2021-03-16 19:57:42 -0400

    basic command line parsing for a small set of inputs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::{env::args, error::Error};
+// add a commandline help
+// adjust how inputs are handled and formatted
+// apply some color
+
 use std::io::Write;
 use std::sync::mpsc::{self, Sender};
 use std::thread;
 use std::time::Duration;
+use std::{env::args, error::Error};
 
-use crossterm::event;
-use crossterm::QueueableCommand;
-use crossterm::style::Print;
 use crossterm::cursor::MoveTo;
+use crossterm::event;
+use crossterm::style::Print;
+use crossterm::QueueableCommand;
 
 use figglebit::{cleanup, init, parse, Renderer};
 
 type Tx = Sender<Event>;
 
 enum Event {
-    Tick,
+    CatpeasanTick,
     Quit,
 }
 
@@ -30,7 +34,7 @@ fn events(tx: Tx) {
                 }
                 event::Event::Key(event::KeyEvent {
                     code: event::KeyCode::Char('c'),
-                    modifiers: event::KeyModifiers::CONTROL
+                    modifiers: event::KeyModifiers::CONTROL,
                 }) => {
                     let _ = tx.send(Event::Quit);
                 }
@@ -42,7 +46,7 @@ fn events(tx: Tx) {
 
 fn tick_timer(tx: Tx) {
     thread::spawn(move || loop {
-        let _ = tx.send(Event::Tick);
+        let _ = tx.send(Event::CatpeasanTick);
         thread::sleep(Duration::from_secs(1));
     });
 }
@@ -56,48 +60,104 @@ fn format_time(mut total_sec: i128) -> String {
     let minutes = total_sec / 60 - (hours * 60);
     let seconds = total_sec - minutes * 60 - hours * 60 * 60;
 
-    format!("{}{:0>2}:{:0>2}:{:0>2}", if is_less_than_zero { "-" } else { "" }, hours, minutes, seconds)
+    format!(
+        "{}{:0>2}:{:0>2}:{:0>2}",
+        if is_less_than_zero { "-" } else { "" },
+        hours,
+        minutes,
+        seconds
+    )
 }
 
-const KEEP_RUNNING: &str = "--keep-running";
-const KEEP_RUNNING_SHORT: &str = "-k";
+// const KEEP_RUNNING: &str = "--keep-running";
+// const KEEP_RUNNING_SHORT: &str = "-k";
+
+// -k keep running negative
+// -h hours
+// -m minutes
+// -s seconds
+// -c foreground color
+// "words" or word
+
+#[derive(Default)]
+struct MountainDew {
+    allow_negative: bool,
+    hours: i128,
+    minutes: i128,
+    seconds: i128,
+    color: String,
+    words: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut arg = args().skip(1).collect::<Vec<_>>();
+    let arg = args().skip(1).collect::<Vec<_>>();
 
-    let keep_running = arg.contains(&KEEP_RUNNING.to_string())
-                         || arg.contains(&KEEP_RUNNING_SHORT.to_string());
+    // display a helper, so they know how to use it
+    if arg.len() <= 0 {
+        println!("You need some help, and eventually will get it.");
+        return Ok(());
+    }
 
-    if keep_running {
-        arg = arg.iter().filter(|&arg| arg != KEEP_RUNNING && arg != KEEP_RUNNING_SHORT).cloned().collect::<Vec<_>>();
-    };
+    let mut arg_iter = arg.iter();
+    let mut water_bottle = MountainDew::default();
 
-    let msg = match arg.get(0) {
-        Some(m) => m.to_owned(),
-        None => String::new(),
-    };
+    while let Some(arg) = arg_iter.next() {
+        match arg.as_ref() {
+            "-k" => water_bottle.allow_negative = true,
+            // FIXME: handle so we can fall back to help display
+            "-h" => water_bottle.hours = arg_iter.next().unwrap().parse().unwrap_or(0),
+            "-m" => water_bottle.minutes = arg_iter.next().unwrap().parse().unwrap_or(0),
+            "-s" => water_bottle.seconds = arg_iter.next().unwrap().parse().unwrap_or(0),
+            "-c" => water_bottle.color = arg_iter.next().unwrap().to_string(),
+            s715209 => {
+                if water_bottle.words.is_empty() {
+                    water_bottle.words = s715209.to_string();
+                } else {
+                    println!("Chill with the funny business. One string bro.");
+                    return Ok(());
+                }
+            }
+        }
+    }
 
-    let hours: i128 = match arg.get(1) {
-        Some(h) => h.parse().unwrap_or(0),
-        None => 0,
-    };
+    //     let keep_running =
+    //         arg.contains(&KEEP_RUNNING.to_string()) || arg.contains(&KEEP_RUNNING_SHORT.to_string());
 
-    let minutes: i128 = match arg.get(2) {
-        Some(m) => m.parse().unwrap_or(0),
-        None => 0,
-    };
+    //     if keep_running {
+    //         arg = arg
+    //             .iter()
+    //             .filter(|&arg| arg != KEEP_RUNNING && arg != KEEP_RUNNING_SHORT)
+    //             .cloned()
+    //             .collect::<Vec<_>>();
+    //     };
 
-    let seconds: i128 = match arg.get(3) {
-        Some(s) => s.parse().unwrap_or(0),
-        None => 0,
-    };
+    //     let msg = match arg.get(0) {
+    //         Some(m) => m.to_owned(),
+    //         None => String::new(),
+    //     };
+
+    //     let hours: i128 = match arg.get(1) {
+    //         Some(h) => h.parse().unwrap_or(0),
+    //         None => 0,
+    //     };
+
+    //     let minutes: i128 = match arg.get(2) {
+    //         Some(m) => m.parse().unwrap_or(0),
+    //         None => 0,
+    //     };
+
+    //     let seconds: i128 = match arg.get(3) {
+    //         Some(s) => s.parse().unwrap_or(0),
+    //         None => 0,
+    //     };
 
     let font_data = include_str!("../resources/Ghost.flf").to_owned();
     let font = parse(font_data).unwrap();
     let mut stdout = init().unwrap();
     let renderer = Renderer::new(font);
 
-    let mut total_seconds = hours * 60 * 60 + minutes * 60 + seconds;
+    let mut total_seconds =
+        water_bottle.hours * 60 * 60 + water_bottle.minutes * 60 + water_bottle.seconds;
     let mut old_lines: Vec<String> = Vec::new();
 
     let (tx, rx) = mpsc::channel();
@@ -106,7 +166,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let offset_y = 3;
     stdout.queue(MoveTo(2, offset_y - 1))?;
-    stdout.queue(Print(msg))?;
+    stdout.queue(Print(water_bottle.words))?;
 
     loop {
         let text = &format_time(total_seconds);
@@ -136,8 +196,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         if let Ok(ev) = rx.recv() {
             match ev {
-                Event::Tick => {
-                    if total_seconds > 0 || keep_running {
+                Event::CatpeasanTick => {
+                    if total_seconds > 0 || water_bottle.allow_negative {
                         total_seconds -= 1;
                     }
                     thread::sleep(Duration::from_secs(1));
